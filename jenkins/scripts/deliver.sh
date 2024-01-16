8563c8dac63f0e2f92542ffe5ab9e73ed71b5885
JAYESH KAPSE <127210074+jayeshkapse@users.noreply.github.com>
2024-01-16 15:52:55 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+echo 'Installing Maven-built Java application into the local Maven repository...'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+mvn clean install
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+echo 'Extracting project name from pom.xml...'
 set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=$(mvn help:evaluate -Dexpression=project.name | grep "^[^\[]")
 set +x
 
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
+echo 'Extracting project version from pom.xml...'
 set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
 set +x
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+echo 'Running the Java application...'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+set +x
